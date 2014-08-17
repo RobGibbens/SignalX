40d7497c8ab24d800ccb3cd99e9c72d940f8873a
Rob Gibbens <RobGibbens@arteksoftware.com>
2014-08-17 13:40:27 -0400

    Added Windows Phone projet

---
+++ SignalX.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Phone.Controls;
+using Xamarin.Forms;
+
+namespace SignalX.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = SignalX.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
